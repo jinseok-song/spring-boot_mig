6f641c28c42ad5824484d636bacec41867be0073
jinseok-song <paso0129@jongja.net>
2021-06-08 18:32:55 +0900

    ignore

---
+++ src/main/java/net/appspeed/studio/Application.java
@@ -0,0 +1,39 @@
+package net.appspeed.studio;
+
+import java.util.Arrays;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.ApplicationContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+
+@SpringBootApplication
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+public class Application {
+	public static final Logger logger = LoggerFactory.getLogger(Application.class);
+	
+	
+	public static void main(String[] args) {
+		ApplicationContext ctx = SpringApplication.run(Application.class, args);
+
+		System.out.println("Let's inspect the beans provided by Spring Boot:");
+	
+//
+//		String[] beanNames = ctx.getBeanDefinitionNames();
+//		Arrays.sort(beanNames);
+//		for (String beanName : beanNames) {
+//			System.out.println(beanName);
+//
+//		}
+	
+		
+		
+	}
+	
+ 
+}
